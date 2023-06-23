5f58034797be7deea9ca88fc2f56a8c0e8f5c793
alexaengland <alexaengland29@utexas.edu>
2023-06-23 16:16:53 +1000

    Finished lab

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "../assets/Alexa_Picture.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        {/*<p>Design your About me page </p> */}
+        <div class="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={profile_pic}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Alexa England</div>
+            <div className="brief_description">
+              I am a rising junior at the University of Texas at Austin, majoring in Electrical and Computer Engineering.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
